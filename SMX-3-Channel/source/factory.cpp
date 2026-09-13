a04390b46b8db5c5d93c7fc8892874e02e8bf9c2
challanger2000 <challanger2000@gmx.de>
2026-09-14 00:32:25 +0200

    Set SMX-3 Channel public vendor metadata to 125A

--- SMX-3-Channel/source/factory.cpp
+++ SMX-3-Channel/source/factory.cpp
@@ -10,8 +10,8 @@
 using namespace Steinberg;
 using namespace Steinberg::Vst;
 
-BEGIN_FACTORY_DEF("challanger2000",
-                  "https://github.com/challanger2000/SaturatorMixFX",
+BEGIN_FACTORY_DEF("125A",
+                  "https://github.com/challanger2000/SaturatorMixFX-Final",
                   "")
 
 DEF_CLASS2(INLINE_UID_FROM_FUID(SaturatorMixFX::kProcessorUID),
